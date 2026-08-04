84e633e87a30fdee51c5536aeef030e45f567efe
MarcosOkamoto1 <marcosviniciusokamoto2003@outlook.com>
2026-08-04 17:22:25 -0400

    Initial Commit

---
+++ SwiftStudyCoach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftStudyCoach
+//
+//  Created by Marcos Vinicius Alencar de Souza Okamoto on 04/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftStudyCoach/SwiftStudyCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudyCoachApp.swift
+//  SwiftStudyCoach
+//
+//  Created by Marcos Vinicius Alencar de Souza Okamoto on 04/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudyCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
